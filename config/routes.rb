94b916f9b2f696fedf39a6c5352bbba2f01a8522
Iroha71 <y.koba.h1071@outlook.jp>
2019-11-24 22:55:36 +0900

    ➕秘書設定API #3

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: 'auth/registrations', confirmations: 'auth/confirmations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :girl
+  resources :user
 end
