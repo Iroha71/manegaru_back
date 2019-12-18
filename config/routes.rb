eac3f36aafc66bc6cf011c37a8061f26307b830d
Iroha71 <y.koba.h1071@outlook.jp>
2019-12-18 14:26:48 +0900

    ➕秘書開放API #4

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :girl
   resources :user
+  resources :user_girl
 end
