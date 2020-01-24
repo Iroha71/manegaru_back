f523b5d6c04607805c950a1d0f4f00f9c3eac5af
Iroha71 <y.koba.h1071@outlook.jp>
2020-01-24 12:14:27 +0900

    ➕タスク取得API #9

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,5 +1,23 @@
 class TaskController < ApplicationController
     before_action :authenticate_user!
+
+    def index
+        if params[:group_id].present?
+            @task = Task.includes(:priority).includes(:project).where(user_id: @current_user.id, project_id: params[:group_id])
+        else
+            @task = Task.includes(:priority).includes(:project).where(user_id: @current_user.id)
+        end
+        render status: 200, json: @task
+    end
+
+    def show
+        @task = Task.includes(:priority).includes(:project).find_by(user_id: @current_user.id, id: params[:id])
+        if @task
+            render status: 200, json: @task
+        else
+            render status: 404, json: { error: 'このタスクは存在しません' }
+        end
+    end
     
     def create
         @task = Task.new(get_task_params)
