148d1a158d9eea00d3ccfb953d6f8964698cf1d1
Iroha71 <y.koba.h1071@outlook.jp>
2020-02-11 09:54:49 +0900

    🔼バリデーション追記 #12

--- app/models/user_girl.rb
+++ app/models/user_girl.rb
@@ -2,6 +2,9 @@ class UserGirl < ApplicationRecord
   belongs_to :user
   belongs_to :girl
 
+  validates :user_id, presence: true
+  validates :girl_id, presence: true
+
   def self.get_new_girl(params, is_paid)
     if is_paid  
       @user_girl = self.new(params)
