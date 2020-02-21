54beead1a692c47d19140a076fa4739e5467d5b9
Iroha71 <y.koba.h1071@outlook.jp>
2020-02-21 22:41:09 +0900

    🔄フロントURL使用部分の環境変数を統一 #10

--- app/controllers/auth/confirmations_controller.rb
+++ app/controllers/auth/confirmations_controller.rb
@@ -3,9 +3,9 @@ module DeviseTokenAuth
         def show
             @resources = resource_class.confirm_by_token(params[:confirmation_token])
             if @resources.errors.empty?
-                redirect_to(ENV['CONFIRM_URL'] + "?confirmedEmail=#{@resources.email}")
+                redirect_to(ENV['CLIENT_URL'] + 'user/confirmed' + "?confirmedEmail=#{@resources.email}")
             else
-                redirect_to(ENV['CONFIRM_URL'] + "?error=500")
+                redirect_to(ENV['CLIENT_URL'] + 'user/confirmed' + "?error=500")
             end
         end
     end
