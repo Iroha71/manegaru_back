1e0d5bbea90a98f491b5f004997c50cc4ce8dbaa
Iroha71 <y.koba.h1071@outlook.jp>
2020-02-29 16:56:53 +0900

    ➕所持金返却API #22

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: 'auth/registrations', confirmations: 'auth/confirmations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :girl
-  resources :user
+  resources :user do
+    collection do
+      get 'get_gold'
+    end
+  end
   resources :user_girl
   resources :project
   resources :task do
