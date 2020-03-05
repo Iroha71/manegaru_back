b1ca571ea27aa873935c0bbc430daa46b5cf7d93
Iroha71 <y.koba.h1071@outlook.jp>
2020-03-05 13:49:24 +0900

    🔄タイポの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform.deliveries = true
+  config.action_mailer.perform_deliveries  = true
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.sendgrid.net',
