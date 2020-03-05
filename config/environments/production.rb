b42d21c3138797047078e1a9d1962c348b807af1
Iroha71 <y.koba.h1071@outlook.jp>
2020-03-05 13:46:40 +0900

    🔄足りないメール設定を追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform.deliveries = true
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.sendgrid.net',
@@ -68,7 +69,7 @@ Rails.application.configure do
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
-    :tls => true
+    :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
