1ca39b56a823932052f30ab18ffa11b1fe8e0bb4
Iroha71 <y.koba.h1071@outlook.jp>
2020-03-08 18:11:46 +0900

    🔼LINE BOTと連携済みフラグ追加 #1

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :nickname, :personal_pronoun, :personality, :gold
+  attributes :id, :email, :name, :nickname, :personal_pronoun, :personality, :gold, :is_cooped_line
   belongs_to :girl
+
+  def is_cooped_line
+    object.line_id.present?
+  end
 end
