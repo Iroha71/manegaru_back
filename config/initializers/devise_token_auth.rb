32bbfc3bb185d2dda8ebf096eb774a392891b432
Iroha71 <y.koba.h1071@outlook.jp>
2020-03-12 19:31:02 +0900

    🔄フロントにリダイレクト時、必ずスラッシュを付与するように修正

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -52,6 +52,6 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you can
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   # config.enable_standard_devise_support = false
-  config.default_password_reset_url = ENV['CLIENT_URL'] + '/user/reset-password'
+  config.default_password_reset_url = ENV['CLIENT_URL'] + '/user/reset-password/'
   config.default_confirm_success_url = "confirmed"
 end
