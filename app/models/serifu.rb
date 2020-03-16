d3d0b9f107b3f07610d79fbd6d2b73a4ace61e72
Iroha71 <y.koba.h1071@outlook.jp>
2020-03-16 22:02:04 +0900

    ➕マスター --> ユーザニックネーム変換処理 #28

--- app/models/serifu.rb
+++ app/models/serifu.rb
@@ -1,3 +1,12 @@
 class Serifu < ApplicationRecord
   belongs_to :girl
+
+  def replace_user_name(user)
+    if self.message.index('マスター')
+      self.message = self.message.gsub!(/マスター/u, user.nickname)
+      self
+    else
+      self
+    end
+  end
 end
