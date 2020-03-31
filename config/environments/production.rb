d8ccc45a7d507cfcd99f8db8b64db7193d9a03a3
Iroha71 <y.koba.h1071@outlook.jp>
2020-04-01 00:24:57 +0900

    ♻starttlsの重複を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries  = true
   config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :domain => 'herokuapp.com',
