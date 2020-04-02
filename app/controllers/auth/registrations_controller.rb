5098ab2c62c8a7a9e184976ce3ad54af318d598c
Iroha71 <y.koba.h1071@outlook.jp>
2020-04-03 00:14:16 +0900

    ♻不要なカラムの削除・カラム名の変更

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
     def sign_up_params
-        params.permit(:name, :nickname, :image, :email, :password, :personal_pronoun, :personality, :girl_id)
+        params.permit(:name, :nickname, :image, :email, :password, :personal_pronoun, :girl_id)
     end
 
     def account_update_params
-        params.permit(:name, :nickname, :image, :email, :personal_pronoun, :personality, :girl_id, :notify_method)
+        params.permit(:name, :nickname, :image, :email, :personal_pronoun, :girl_id, :notify_method)
     end
 end
